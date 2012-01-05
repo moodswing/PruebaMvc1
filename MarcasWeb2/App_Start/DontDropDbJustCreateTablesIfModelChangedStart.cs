9c2a82c185c9cc1d3ec8a0317c06f1c1b200e6b4
Robinson Aravena <rob.arav@gmail.com>
2012-01-05 15:52:27 -0300

    databasecontext

---
+++ MarcasWeb2/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity;
+using Devtalk.EF.CodeFirst;
+using MarcasWeb2.Models;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(MarcasWeb2.App_Start.DontDropDbJustCreateTablesIfModelChangedStart), "Start")]
+
+namespace MarcasWeb2.App_Start {
+    public static class DontDropDbJustCreateTablesIfModelChangedStart {
+        public static void Start() {
+            // Uncomment this line and replace CONTEXT_NAME with the name of your DbContext if you are 
+            // using your DbContext to create and manage your database
+            Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<DataBaseContext>());
+        }
+    }
+}
---
+++ MarcasWeb2/Models/DataBaseContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace MarcasWeb2.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
